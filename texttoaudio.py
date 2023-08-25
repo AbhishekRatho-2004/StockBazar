1736b7311a65eba63fddf5452a3a2b0aa3ebfbc6
AbhishekRatho-2004 <134370229+AbhishekRatho-2004@users.noreply.github.com>
2023-08-25 11:43:02 +0530

    Update texttoaudio.py

--- texttoaudio.py
+++ texttoaudio.py
@@ -7,7 +7,7 @@ def speech(text):
     engine = pyttsx3.init()
 
     # Set properties (optional)
-    # engine.setProperty("rate", 150)  # Speed of speech
+    engine.setProperty("rate", 140)
     # engine.setProperty("volume", 0.9)  # Volume level (0.0 to 1.0)
 
     # Convert text to speech and play it
